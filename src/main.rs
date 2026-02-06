e4abbfc6fb0a94dddb35f9a14a7eb47c34eb9dca
somewhatfrog <somewhatfrog@gmail.com>
2026-02-06 23:18:46 +0300

    if else >> match

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,17 @@ fn find_focused_and_parent_layout(node: &Node, parent_layout: NodeLayout) -> Opt
     if node.focused {
         return Some((node.rect, parent_layout));
     }
-    node.nodes.iter()
-        .find_map(|child| find_focused_and_parent_layout(child, node.layout))
+    node.nodes.iter().find_map(|child| find_focused_and_parent_layout(child, node.layout))
 }
 
 fn set_layout(conn: &mut Connection) -> Fallible<()> {
     let tree = conn.get_tree()?;
     if let Some((rect, parent_layout)) = find_focused_and_parent_layout(&tree, tree.layout) {
-        if matches!(parent_layout, NodeLayout::Tabbed | NodeLayout::Stacked) {
-            return Ok(());
-        }
-        if rect.height > rect.width {
-            if parent_layout == NodeLayout::SplitH {
-                conn.run_command("split v")?;
-            }
-        } else if parent_layout == NodeLayout::SplitV {
-            conn.run_command("split h")?;
+        match (rect.height > rect.width, parent_layout) {
+            (_, NodeLayout::Tabbed | NodeLayout::Stacked) => {}
+            (true, NodeLayout::SplitH) => { conn.run_command("split v")?; }
+            (false, NodeLayout::SplitV) => { conn.run_command("split h")?; }
+            _ => {}
         }
     }
     Ok(())
@@ -29,7 +24,6 @@ fn main() -> Fallible<()> {
     // Create connection and sub to events, subscribe() consumes the connection
     let conn = Connection::new()?;
     let events = conn.subscribe(&[EventType::Window])?;
-
     // New connection for sending commands
     let mut cmd_conn = Connection::new()?;
     for event in events {
